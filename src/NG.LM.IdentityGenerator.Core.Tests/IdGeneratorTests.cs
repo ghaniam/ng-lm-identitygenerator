a30d3d584f13806260cb553b16d656ecb622902a
Ghani Malik <ghani.malik@asml.com>
2026-03-17 09:50:46 +0100

    Initial commit: NG.LM.IdentityGenerator library

---
+++ src/NG.LM.IdentityGenerator.Core.Tests/IdGeneratorTests.cs
@@ -0,0 +1,14 @@
+﻿namespace NG.LM.IdentityGenerator.Core.Tests
+{
+    public class IdGeneratorTests
+    {
+        [Fact]
+        public void IdGenerator_ReturnsOk()
+        {
+            var prefix = "TEST";
+            var identifier = IdGenerator.GenerateId(prefix);
+            Assert.NotNull(identifier);
+            Assert.Equal(prefix, identifier.Substring(0, 4));
+        }
+    }
+}
---
+++ src/NG.LM.IdentityGenerator.Core/IdGenerator.cs
@@ -0,0 +1,21 @@
+﻿namespace NG.LM.IdentityGenerator.Core;
+
+public static class IdGenerator
+{
+    public static string GenerateId(string prefix = "ID")
+    {
+        return $"{prefix}-{Guid.NewGuid().ToString().ToUpper()}";
+    }
+
+    public static string GenerateSequentialId(string prefix = "SEQ")
+    {
+        var timestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+        var random = Guid.NewGuid().ToString("N").Substring(0, 8).ToUpper();
+        return $"{prefix}-{timestamp}-{random}";
+    }
+    
+    public static int GenerateNumberFromRange(int start, int count)
+    {
+        return Enumerable.Range(start, count).OrderBy(x => new Random().Next()).First();
+    }
+}
